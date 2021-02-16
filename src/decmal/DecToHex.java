e62f471add812db1d455f82876f6d314d0983140
Karr847 <mohammed_arr@hotmail.com>
2021-02-16 21:21:58 +0000

    Covert Decimal to hex

---
+++ src/decmal/DecToHex.java
@@ -0,0 +1,27 @@
+package decmal;
+
+import java.util.Scanner;
+
+public class DecToHex {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        //prompt the user enter a decimal integer
+        System.out.println("Enter a decimal number:");
+        int decimal=input.nextInt();
+
+        //Convert decimal hex
+        String hex="";
+        while (decimal !=0){
+            int hexValue=decimal%16;
+            //Convert a decimal value to a hex digit
+            char hexDigit=(hexValue<=9 && hexValue>=0)?
+                    (char) (hexValue+'0'):(char) (hexValue-10+'A');
+            hex=hexDigit+hex;
+            decimal=decimal/16;
+        }
+        System.out.println("The hex number is :"+hex);
+
+
+
+    }
+}
